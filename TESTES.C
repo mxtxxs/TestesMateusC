800998bea7a00a6677c25cc86612108f0122a60a
Mateus dos santos Cardoso <ep.mateus.santos@gmail.com>
2025-10-23 22:02:35 -0300

    Add files via upload

---
+++ TESTES.C
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+int idade;
+
+printf("qual sua idade?");
+scanf("%d", & idade);
+
+printf("vc tem (%d) anos\n", idade);
+
+return 0;
+
+}
---
+++ ola mundo.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()  {
+    printf("olá mundo\n");
+    printf("á ã â ç \n");
+return 0;
+
+}
